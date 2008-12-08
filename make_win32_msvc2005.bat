c3b8bc5d903ab92da598b2dee7d1d3fc4f221178
Magnus Auvinen <magnus.auvinen@gmail.com>
2008-12-08 20:12:28 +0000

    better msvc batch file

--- make_win32_msvc2005.bat
+++ make_win32_msvc2005.bat
@@ -1,3 +1,23 @@
+@echo off 
+ 
+:: Check for Visual Studio 
+if exist "%VS90COMNTOOLS%" ( 
+	set VSPATH="%VS90COMNTOOLS%" 
+	goto compile 
+) 
+if exist "%VS80COMNTOOLS%" ( 
+	set VSPATH="%VS80COMNTOOLS%" 
+	goto compile 
+) 
+ 
+echo You need Microsoft Visual Studio 8 or 9 installed 
+pause 
+exit 
+ 
+:compile 
+ 
+call %VSPATH%vsvars32.bat 
+
 @cl /D_CRT_SECURE_NO_DEPRECATE /O2 /nologo src/tools/txt2c.c /Fesrc/tools/txt2c.exe
 @src\tools\txt2c.exe < src\base.bam > src\internal_base.h
 @cl /D_CRT_SECURE_NO_DEPRECATE /W3 /TC /O2 /nologo /I src/lua/include src/lua/src/*.c src/lua/src/lib/*.c src/*.c /Fesrc/bam.exe
