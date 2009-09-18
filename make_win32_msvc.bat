e2f7aa9b4f0e75c6c04cb85574169c1bcd861966
Magnus Auvinen <magnus.auvinen@gmail.com>
2009-09-18 09:02:36 +0000

    fixed the windows compile to compile bam before lua to get errors during development earlier. fixed so it doesn't call vcvars32.bat each time it's launched.

--- make_win32_msvc.bat
+++ make_win32_msvc.bat
@@ -1,25 +1,31 @@
 @echo off
 
-:: Check for Visual Studio
+@REM check if we already have the tools in the environment
+if exist "%VCINSTALLDIR%" (
+	goto compile
+)
+
+@REM Check for Visual Studio
 if exist "%VS90COMNTOOLS%" (
 	set VSPATH="%VS90COMNTOOLS%"
-	goto compile
+	goto set_env
 )
 if exist "%VS80COMNTOOLS%" (
 	set VSPATH="%VS80COMNTOOLS%"
-	goto compile
+	goto set_env
 )
 
 echo You need Microsoft Visual Studio 8 or 9 installed
 pause
 exit
 
-:compile
-
+@ setup the environment
+:set_env
 call %VSPATH%vsvars32.bat
 
+:compile
 @cl /D_CRT_SECURE_NO_DEPRECATE /O2 /nologo src/tools/txt2c.c /Fesrc/tools/txt2c.exe
 @src\tools\txt2c src/base.lua src/driver_gcc.lua src/driver_cl.lua > src\internal_base.h
-@cl /D_CRT_SECURE_NO_DEPRECATE /W3 /TC /O2 /nologo /I src/lua src/lua/*.c src/*.c /Febam.exe
+@cl /D_CRT_SECURE_NO_DEPRECATE /W3 /TC /O2 /nologo /LTGC /I src/lua src/*.c src/lua/*.c /Febam.exe
 @del *.obj
 
