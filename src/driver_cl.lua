88878a9c8a69e00703473b4c94ef0418de381c59
Magnus Auvinen <magnus.auvinen@gmail.com>
2010-01-27 15:06:12 +0100

    fixed missing space in driver_cl dll function

--- src/driver_cl.lua
+++ src/driver_cl.lua
@@ -53,7 +53,7 @@ function DriverDLL_CL(label, output, inputs, settings)
 	local libs  = tbl_to_str(settings.dll.libs, "", " ")
 	local libpaths = tbl_to_str(settings.dll.libpath, "/libpath:\"", "\" ")
 	local exe = str_replace(settings.dll.exe, "/", "\\")
-	local exec = exe .. ' /nologo /DLL' .. flags .. libpaths .. libs .. " /OUT:" .. output .. " " .. input
+	local exec = exe .. " /nologo /DLL " .. flags .. libpaths .. libs .. " /OUT:" .. output .. " " .. input
 	AddJob(output, label, exec)
 	--return exec
 end
