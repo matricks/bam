261a4c760f7b23ddcf36b9728fab8186b1330c20
Markus Alind <markus.alind@machinegames.com>
2016-09-22 11:30:54 +0200

    compile with define LUA_USE_POSIX on unix to get popen in lua (its included on windows)

--- make_unix.sh
+++ make_unix.sh
@@ -25,4 +25,4 @@ fi
 echo "compiling using $CC..." >&2
 $CC -Wall -pedantic src/tools/txt2c.c -o src/tools/txt2c
 src/tools/txt2c src/base.lua src/tools.lua src/driver_gcc.lua src/driver_clang.lua src/driver_cl.lua src/driver_solstudio.lua src/driver_xlc.lua > src/internal_base.h
-$CC -Wall -pedantic src/*.c src/lua/*.c -o bam -I src/lua -lm -lpthread $ldl -O2 -rdynamic $*
+$CC -Wall -DLUA_USE_POSIX -pedantic src/*.c src/lua/*.c -o bam -I src/lua -lm -lpthread $ldl -O2 -rdynamic $*
