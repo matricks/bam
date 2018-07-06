10867094120a2473f5443077d07491af0c4dc475
Markus Alind <markus.alind@machinegames.com>
2018-07-06 09:57:28 +0200

    Added -DLUA_USE_POSIX to the other unix-y make-scripts as well (make_aix.sh, make_beos.sh, make_hpus.sh, make_solaris.sh), but I can't test those.

--- make_solaris.sh
+++ make_solaris.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cc src/tools/txt2c.c -o src/tools/txt2c
 src/tools/txt2c src/base.lua src/tools.lua src/driver_gcc.lua src/driver_cl.lua src/driver_clang.lua src/driver_solstudio.lua src/driver_xlc.lua > src/internal_base.h
-cc src/*.c src/lua/*.c -o bam -I src/lua -lm -lrt -lpthread -ldl -mt -O2 -std=c99 $*
+cc src/*.c src/lua/*.c -o bam -DLUA_USE_POSIX -I src/lua -lm -lrt -lpthread -ldl -mt -O2 -std=c99 $*
