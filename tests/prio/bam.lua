1795f80d0349782256880790e4e6bc35819b36b7
Markus Alind <markus.alind@machinegames.com>
2023-02-21 14:22:32 +0100

    Added a multi-output-job to prio test (does not work reasonably with current implementation).

--- tests/prio/bam.lua
+++ tests/prio/bam.lua
@@ -3,6 +3,10 @@ function QuickJob(name, deps)
 	AddJob(name, name, "echo > " .. name, deps)
 end
 
+function QuickJob2x(name0, name1, deps)
+	AddJob({name0, name1}, "2x " .. name0 .. " " .. name1,  "echo > " .. name0 .. " && echo > " .. name1, deps)
+end
+
 QuickJob("A_common2", {})
 QuickJob("A_common1", {"A_common2"})
 QuickJob("A_a", {"A_common1"})
@@ -11,11 +15,14 @@ ModifyPriority("A_a", 100)
 
 QuickJob("B_common2", {})
 QuickJob("B_common1", {"B_common2"})
-QuickJob("B_a", {"B_common1"})
-QuickJob("B_b", {"B_common1"})
+
+-- multi output is tricky here as multiple nodes references the same job-struct
+QuickJob2x("B_a", "B_b", {"B_common1", "B_common1"})
 ModifyPriority("B_a", 100)
 
 QuickJob("C", {"A_common2", "B_common1"})
 
-QuickJob("top", {"A_a", "A_b", "B_b", "B_a"})
+QuickJob("D", {"B_a"})
+
+QuickJob("top", {"A_a", "A_b", "B_b", "B_a", "D"}) -- order of deps should not affect priorities
 DefaultTarget("top")
